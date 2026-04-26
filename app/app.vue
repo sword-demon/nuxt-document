d7462c17b963741f82ea77483261874b75c90413
王鑫123wangxin <1418667580@qq.com>
2026-04-27 01:42:13 +0800

    feat: add toast position config and login request/response interceptors

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <UApp>
+  <!-- 错误消息配置位置和样式 -->
+  <UApp :toaster="{ position: 'top-center', expand: false }">
     <!-- 默认使用 default.vue 布局 -->
     <NuxtLayout>
       <!-- 默认加载 index.vue -->
--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -42,7 +42,28 @@ async function onSubmit(payload: FormSubmitEvent<Schema>) {
     headers: {
       'content-type': 'application/json'
     },
-    body: payload.data
+    body: payload.data,
+    // 请求拦截器
+    onRequest: () => {
+      // 设置 token
+    },
+    // 响应拦截器
+    onResponse: res => {
+      // console.log('响应处理')
+      console.log('onResponse: ', res)
+      if (res.response.ok) {
+        console.log('操作成功')
+      }
+    },
+    onRequestError: () => {
+      console.log('请求发生错误')
+      toast.add({
+        title: '网络连接错误'
+      })
+    },
+    onResponseError: err => {
+      console.log('onResponseError', err)
+    }
   })
 
   // console.log(res.data)
